f00fb403b3fad2b89fd853380927199c63ab2d37
Toyokazu Akiyama <toyokazu@gmail.com>
2011-04-01 11:57:02 +0900

    add debug mode (suppress to output executed command)

--- make_csr.rb
+++ make_csr.rb
@@ -28,7 +28,8 @@ end
 # - ~/Downloads/bar
 # key_file: ./new_key.pem
 # req_file: ./new_req.pem
-# req_type: csr
+# req_type: csr | x509
+# req_days: 30 (only for x509)
 # cert_descriptions:
 #   C: JP
 #   ST: .
@@ -37,6 +38,7 @@ end
 #   OU: Cybermedia Center
 #   CN: idp01.auth.cmc.osaka-u.ac.jp
 #   emailAddress: .
+# debug: true
 
 # load config.yml file
 config_file = nil
@@ -46,7 +48,7 @@ else
   config_file = ARGV[0]
 end
 
-config = {}
+$config = {}
 if !File.exist?(config_file)
   puts "config file #{config_file} is not found! using default values."
   puts "key_type: rsa"
@@ -57,15 +59,16 @@ if !File.exist?(config_file)
   puts "req_file: new_req.pem"
   puts "req_type: csr"
 else
-  config = YAML.load_file(config_file)
+  $config = YAML.load_file(config_file)
 end
-config["key_type"] ||= "rsa"
-config["key_size"] ||= 2048
-config["random_files"] ||= ["/etc/"]
-config["key_file"] ||= "new_key.pem"
-config["req_file"] ||= "new_req.pem"
+$config["key_type"] ||= "rsa"
+$config["key_size"] ||= 2048
+$config["random_files"] ||= ["/etc/"]
+$config["key_file"] ||= "new_key.pem"
+$config["req_file"] ||= "new_req.pem"
+$config["debug"] ||= false
 # certificate description defaults
-config["cert_descriptions"] = {
+$config["cert_descriptions"] = {
   "C" => "JP",
   "ST" => ".",
   "L" => "Academe2",
@@ -73,14 +76,14 @@ config["cert_descriptions"] = {
   "OU" => ".",
   "CN" => "idp.example.org",
   "emailAddress" => "."
-}.merge(config["cert_descriptions"])
+}.merge($config["cert_descriptions"])
 
 # === obtain random_files
 # If the specified file is a directory, obtain files from
 # the directory and add to the random_files. Finally,
 # select 3 files from random_files.
 random_files = []
-Find.find(*config["random_files"]) do |f|
+Find.find(*$config["random_files"]) do |f|
   if FileTest.file?(f)
     random_files << f
   end
@@ -92,93 +95,106 @@ if random_files.size < 3
 end
 
 # === select 3 files from random_files
-selected_files = []
+$selected_files = []
 (1..3).each do |i|
-  selected_files << random_files[rand(random_files.size)]
+  $selected_files << random_files[rand(random_files.size)]
 end
 
-sh = Shell.new
+$sh = Shell.new
+
+def sh_exec(&block)
+  if $config["debug"]
+    puts "debug mode"
+    $sh.out(STDOUT, &block)
+  else
+    yield(block)
+  end
+end
 
 # === create new rsa key
-puts "creating new key #{config["key_file"]}"
+puts "creating new key #{$config["key_file"]}"
 
-password = nil
-while password.nil?
-  password = prompt("Password:", false)
+$password = nil
+while $password.nil?
+  $password = prompt("Password:", false)
   check_password = prompt("Retype Password:", false)
-  if password != check_password
-    password = nil
+  if $password != check_password
+    $password = nil
   end
 end
 
 null_password = false
-if password.empty?
+if $password.empty?
   null_password = true
-  password = "1234ABCD"
+  $password = "1234ABCD"
 end
 
-sh.out(STDOUT) do
+sh_exec do
   args = [
     "openssl",
-    "gen#{config["key_type"]}",
+    "gen#{$config["key_type"]}",
     "-des3", 
-    "-rand", selected_files.join(File::PATH_SEPARATOR),
-    "-out", config["key_file"],
-    "-passout", "pass:#{password}",
-    config["key_size"].to_s
+    "-rand", $selected_files.join(File::PATH_SEPARATOR),
+    "-out", $config["key_file"],
+    "-passout", "pass:#{$password}",
+    $config["key_size"].to_s
   ]
   system(*args)
 end
 
 if null_password
-  sh.out(STDOUT) do
+  sh_exec do
     args = [
       "openssl",
-      config["key_type"],
-      "-in", config["key_file"],
-      "-passin", "pass:#{password}",
-      "-out", "tmp_#{config["key_file"]}"
+      $config["key_type"],
+      "-in", $config["key_file"],
+      "-passin", "pass:#{$password}",
+      "-out", "tmp_#{$config["key_file"]}"
     ]
     system(*args)
   end
-  FileUtils.mv("tmp_#{config["key_file"]}", config["key_file"])
+  FileUtils.mv("tmp_#{$config["key_file"]}", $config["key_file"])
 end
 
 # === create new csr
-case config["req_type"]
+case $config["req_type"]
 when "csr"
-  puts "creating new csr #{config["req_file"]}"
+  puts "creating new csr #{$config["req_file"]}"
 when "x509"
-  puts "creating new self-signed certificate #{config["req_file"]}"
+  puts "creating new self-signed certificate #{$config["req_file"]}"
 end
 
-subject = ""
+$subject = ""
 
 # describe required dn keys in the order in openssl.conf
 dn_keys = [ "C", "ST", "L", "O", "OU", "CN", "emailAddress" ]
 
 dn_keys.each do |key|
-  if config["cert_descriptions"][key] != '.' &&
-      !config["cert_descriptions"][key].nil?
-    subject = "#{subject}/#{key}=#{config["cert_descriptions"][key]}"
+  if $config["cert_descriptions"][key] != '.' &&
+      !$config["cert_descriptions"][key].nil?
+    $subject = "#{$subject}/#{key}=#{$config["cert_descriptions"][key]}"
   end
 end
 
-sh.out(STDOUT) do
+sh_exec do
   args = [
     "openssl",
     "req",
     "-new",
-    "-subj", subject,
-    "-key", config["key_file"],
-    "-out", config["req_file"]
+    "-subj", $subject,
+    "-key", $config["key_file"],
+    "-out", $config["req_file"]
   ]
   if !null_password
     args << "-passin"
-    args << "pass:#{password}"
+    args << "pass:#{$password}"
   end
-  if config["req_type"] == "x509"
+  if $config["req_type"] == "x509"
     args << "-x509"
+    if !$config["req_days"].nil?
+      args << "-days"
+      args << $config["req_days"].to_s
+    end
   end
   system(*args)
 end
